89225995c32de8c5059d616482ccf2c25f2ea116
DAXXSIR <137784719+DAXXSIR@users.noreply.github.com>
2023-07-30 16:23:10 +0530

    𝐃𝐀𝐗𝐗

--- Config.py
+++ Config.py
@@ -4,8 +4,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Get it from my.telegram.org
-API_ID = getenv("API_ID")
-API_HASH = getenv("API_HASH")
+API_ID = "25450075"
+API_HASH = "278e22b00d6dd565c837405eda49e6f2"
 
 # Get it from @Botfather in Telegram.
-BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_TOKEN = "6643363711:AAEUd8CMNwfd2GdeO5ejpPSkvDP7kX47x3o"
